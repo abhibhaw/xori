704c56aa47faf163474be9744a0ed58253d1350f
Abhibhaw <39991296+abhibhaw@users.noreply.github.com>
2024-02-10 14:00:01 +0530

    feat: Adds channel create and role command, adds winston

--- app.js
+++ app.js
@@ -1,87 +1,63 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
+import 'dotenv/config';
+
+import {Client, IntentsBitField} from 'discord.js';
+import createChannelAndRole from './commands/create.js';
+import winston from './config/winston.js';
+
+const logger = winston('app.js');
+
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.MessageContent,
   ],
 });
 
-const { token, prefix, category } = process.env;
+const {token, prefix, category} = process.env;
 
-client.once("ready", () => {
-  console.log("Bot is online!");
+client.once('ready', () => {
+  logger.info('Bot is online!');
 });
 
-client.on("messageCreate", async (message) => {
+client.on('guildCreate', guild => {
+  // This event triggers when the bot joins a guild.
+  logger.info(`Joined new guild: ${guild.name}`);
+});
+
+client.on('messageCreate', async message => {
   if (message.author.bot || !message.content.startsWith(prefix)) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === "create") {
-    const roleName = args.join(" ");
-    const guild = message.guild;
-    const categoryChannel = guild.channels.cache.find(
-      (channel) =>
-        channel.type === "GUILD_CATEGORY" && channel.name === category
-    );
-
-    if (!categoryChannel) {
-      return message.channel.send("Category not found.");
-    }
-
-    try {
-      const role = await guild.roles.create({
-        name: roleName,
-        color: "RANDOM",
-      });
-
-      const channel = await guild.channels.create(roleName, {
-        type: "GUILD_TEXT",
-        parent: categoryChannel,
-        permissionOverwrites: [
-          {
-            id: role.id,
-            allow: ["VIEW_CHANNEL"],
-          },
-        ],
-      });
-
-      message.channel.send(
-        `Role '${role.name}' and channel '${channel.name}' created successfully.`
-      );
-    } catch (error) {
-      console.error("Error creating role and channel:", error);
-      message.channel.send(
-        "An error occurred while creating role and channel."
-      );
-    }
-  } else if (command === "add_role") {
-    const roleName = args.join(" ");
-    const role = message.guild.roles.cache.find(
-      (role) => role.name === roleName
-    );
+  if (command === 'create') {
+    await createChannelAndRole(category, message, args);
+  } else if (command === 'add_role') {
+    const roleName = args.join(' ');
+    const role = message.guild.roles.cache.find(r => r.name === roleName);
 
     if (!role) {
-      return message.channel.send("Role not found.");
+      message.channel.send('Role not found.');
+      return;
     }
 
     const member = message.mentions.members.first();
 
     if (!member) {
-      return message.channel.send("Member not found.");
+      message.channel.send('Member not found.');
+      return;
     }
 
     try {
       await member.roles.add(role);
       message.channel.send(
-        `Role '${role.name}' added to ${member.displayName} successfully.`
+        `Role '${role.name}' added to ${member.displayName} successfully.`,
       );
     } catch (error) {
-      console.error("Error adding role to member:", error);
-      message.channel.send("An error occurred while adding role to member.");
+      logger.error('Error adding role to member:', error);
+      message.channel.send('An error occurred while adding role to member.');
     }
   }
 });
---
+++ commands/create.js
@@ -0,0 +1,70 @@
+import {PermissionsBitField, ChannelType} from 'discord.js';
+import winston from '../config/winston.js';
+
+const logger = winston('create.js');
+
+const createChannelAndRole = async (category, message, args) => {
+  const roleName = args.join(' ').toLowerCase().replace(' ', '-');
+  const {guild} = message;
+  const categoryChannel = guild.channels.cache.find(
+    channel =>
+      channel.type === ChannelType.GuildCategory && channel.name === category,
+  );
+
+  if (!categoryChannel) {
+    return message.channel.send('Category not found.');
+  }
+
+  try {
+    const role = await guild.roles.create({
+      name: roleName,
+      color: 'Blue',
+      permissions: [
+        PermissionsBitField.Flags.ViewChannel,
+        PermissionsBitField.Flags.SendMessages,
+        PermissionsBitField.Flags.ReadMessageHistory,
+        PermissionsBitField.Flags.EmbedLinks,
+        PermissionsBitField.Flags.AttachFiles,
+        PermissionsBitField.Flags.AddReactions,
+        PermissionsBitField.Flags.UseExternalEmojis,
+      ],
+      reason: `Role for ${roleName} story.`,
+    });
+    logger.info('Role created:', role.name, categoryChannel.id);
+    const channel = await guild.channels.create({
+      name: roleName,
+      type: ChannelType.GuildText,
+      parent: categoryChannel.id,
+      permissionOverwrites: [
+        {
+          id: guild.roles.everyone.id, // Deny permissions for @everyone role
+          deny: [PermissionsBitField.Flags.ViewChannel],
+        },
+        {
+          id: role.id,
+          allow: [
+            PermissionsBitField.Flags.ViewChannel,
+            PermissionsBitField.Flags.SendMessages,
+            PermissionsBitField.Flags.ReadMessageHistory,
+            PermissionsBitField.Flags.EmbedLinks,
+            PermissionsBitField.Flags.AttachFiles,
+            PermissionsBitField.Flags.AddReactions,
+            PermissionsBitField.Flags.UseExternalEmojis,
+          ],
+        },
+      ],
+    });
+
+    logger.info('Channel created:', channel.name);
+    return message.channel.send(
+      `Role '${role.name}' and channel '${channel.name}' created successfully.`,
+    );
+  } catch (error) {
+    logger.error('Error creating role and channel:', error);
+    return message.channel.send(
+      'An error occurred while creating role and channel.',
+    );
+  }
+};
+
+export default createChannelAndRole;
